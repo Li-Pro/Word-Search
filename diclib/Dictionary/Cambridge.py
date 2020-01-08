ffd3d8a33faede699726b479801fe7597607fe68
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2020-01-08 14:42:34 +0800

    Imporved support of Cambridge Dictionary.

--- diclib/Dictionary/Cambridge.py
+++ diclib/Dictionary/Cambridge.py
@@ -7,21 +7,23 @@ def CambridgeParser(soup, bWithExample):
 	""" The parser of Cambridge Dictionary. """
 	rep = dicTypes.Result(bWithExample)
 	
-	for pdef in soup.find_all('div', class_='sense-body'):
-		defs = pdef.find_all('div', class_='def-block', recursive=False)
-		for i in range(len(defs)):
-			defx = []
-			for dat in defs[i].find_all(class_=['di-title', 'posgram', 'anc-info-head', 'def-info', 'def']):
-				defx.append(dat.get_text())
-			
-			rep.defs.append(defx)
-			
-			if bWithExample:
-				exmps = []
-				for exmp in defs[i].find_all('div', class_='examp'):
-					exmps.append(exmp.get_text())
+	for ppdef in soup.find_all('div', class_='entry-body__el'):
+		entry_info = ' '.join([tg.get_text() for tg in ppdef.find_all(class_=['posgram', 'anc-info-head'])])
+		for pdef in ppdef.find_all('div', class_='sense-body'):
+			defs = pdef.find_all('div', class_='def-block', recursive=False)
+			for i in range(len(defs)):
+				defx = []
+				defx.append( entry_info + ' ' + defs[i].find(class_='def-info').get_text() )
+				defx.append(defs[i].find(class_='def').get_text())
 				
-				rep.examples.append(exmps)
+				rep.defs.append(defx)
+				
+				if bWithExample:
+					exmps = []
+					for exmp in defs[i].find_all('div', class_='examp'):
+						exmps.append(exmp.get_text())
+					
+					rep.examples.append(exmps)
 		
 	return rep
 
