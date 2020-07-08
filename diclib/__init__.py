4c2a2430276e78986fda0b3d8f8e574651fbd6ec
Li-Pro <53344765+Li-Pro@users.noreply.github.com>
2020-07-08 17:36:52 +0800

    Adding .editorconfig, updating diclib.

--- diclib/__init__.py
+++ diclib/__init__.py
@@ -6,21 +6,27 @@ The main file of dictionary library.
 
 import string
 import requests
+
 from bs4 import BeautifulSoup
 
 """ The dictionaries """
-import diclib.Dictionary.OxfordLearners as dicOxfordLearners
-import diclib.Dictionary.Urban as dicUrban
-import diclib.Dictionary.Cambridge as dicCambridge
+from .Dictionary import OxfordLearners as dicOxfordLearners
+from .Dictionary import Urban as dicUrban
+from .Dictionary import Cambridge as dicCambridge
 
 # The extensible dictionary list!
-dic_list = {'oed': dicOxfordLearners.DIC_OBJ, 'urb': dicUrban.DIC_OBJ, 'camb': dicCambridge.DIC_OBJ}
+dic_list = {'oed': dicOxfordLearners.DIC_OBJ,
+			'urb': dicUrban.DIC_OBJ,
+			'camb': dicCambridge.DIC_OBJ}
 
 def getWordPage(key, dic_obj):
 	""" Send request towards the online dictionary. """
 	url = ''
-	if type(dic_obj.urlformat)==str: url = str(dic_obj.urlformat % (key))
-	elif callable(dic_obj.urlformat): url = dic_obj.urlformat(key)
+	if type(dic_obj.urlformat)==str:
+		url = str(dic_obj.urlformat % (key))
+	elif callable(dic_obj.urlformat):
+		url = dic_obj.urlformat(key)
+	
 	return requests.get(url).text
 
 def searchWord(key, dicname='camb', bWithExample=False):
